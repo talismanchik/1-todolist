cb452e47bc3597e9024f5018aae9ae81bba6af5c
talismanchik <talismanchik.by@gmail.com>
2022-11-25 19:05:47 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+
+
+
+type propsType={
+    body: string
+    id: number
+    title:string
+    userId: number
+}
+
+
 function App() {
+
+    const [state, setState] = useState<propsType[]>([])
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setState(json))
+    },[])
+
+    const deleteHandler =()=>{
+        setState([])
+
+    }
+    const showPostsHandler =()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setState(json))
+    }
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn!!!</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <button onClick={deleteHandler}>DELETE</button>
+            <button onClick={showPostsHandler}>SHOW POSTS</button>
+            <ul>
+                {state.map((el)=>{
+                    return(
+                        <li key={el.id}>
+                            <span>{el.id}</span>
+                            <span>{el.userId}</span>
+                            <span>{el.body}</span>
+                            <span>{el.title}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+
         </div>
     );
 }
